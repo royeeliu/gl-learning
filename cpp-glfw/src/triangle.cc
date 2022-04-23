5aac3cb2f32e563aaace42aef2ce24ebad79c0aa
royeeliu <royeeliu@qq.com>
2022-04-23 18:56:50 +0800

    add triangle-color

--- cpp-glfw/src/triangle.cc
+++ cpp-glfw/src/triangle.cc
@@ -126,7 +126,7 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
     glEnableVertexAttribArray(0);
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound
--- cpp-glfw/src/triangle_color.cc
+++ cpp-glfw/src/triangle_color.cc
@@ -5,27 +5,34 @@
 const char* const VERTEXT_SHADER_SOURCE = R"(
     #version 330 core
     layout (location = 0) in vec3 aPos;
+    layout (location = 1) in vec3 aColor;
+    out vec3 ourColor;
     uniform float offset;
     void main()
     {
-       gl_Position = vec4(aPos.x + offset, aPos.y, aPos.z, 1.0);
+       gl_Position = vec4(aPos, 1.0);
+       ourColor = vec3(offset, aColor.g, aColor.b);
     }
 )";
 
 const char* const FRAGMENT_SHADER_SOURCE = R"(
     #version 330 core
     out vec4 FragColor;
+    in vec3 ourColor;
     void main()
     {
-        FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+        FragColor = vec4(ourColor, 1.0f);
     }
 )";
 
+// clang-format off
 GLfloat vertices[] = {
-    -0.5f, -0.5f, 0.0f, // left
-    0.5f,  -0.5f, 0.0f, // right
-    0.0f,  0.5f,  0.0f  // top
+    // positions         // colors
+    -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // left
+    0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f, // right
+    0.0f, 0.5f, 0.0f,   0.0f, 0.0f, 1.0f, // top
 };
+// clang-format on
 
 int main()
 {
@@ -51,8 +58,10 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)0);
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound
     // vertex buffer object so afterwards we can safely unbind
@@ -62,13 +71,13 @@ int main()
     // uncomment this call to draw in wireframe polygons.
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-    GLfloat offset = -0.5f;
+    GLfloat offset = 0.0f;
     GLfloat step = 0.005f;
 
     module.SetBackgroundColor(0.2f, 0.3f, 0.3f);
     module.RunMessageLoop([shader_program, vao, &offset, &step] {
         offset += step;
-        if (offset > 0.5f || offset < -0.5f)
+        if (offset > 1.0f || offset < 0.0f)
         {
             step = -step;
         }
